edae3d070de07d5fccbc5b3274fd49aa3c6bee25
TX-Unity <67395769+TX-Unity@users.noreply.github.com>
2020-06-26 11:11:14 -0500

    Update capture-tethered-wifi.sh

--- green/capture-tethered-wifi.sh
+++ green/capture-tethered-wifi.sh
@@ -9,38 +9,28 @@ now=$(date +'%m-%d-%Y.%H.%M.%S')
 now=$(date +'%m-%d-%Y')
 
 
-counter=0
-
-while :
-do
-  counter=0
-  while ! ping -c 1 -n -w 1 $camera_ip &> /dev/null
-  do
-        ((counter++))
-        echo -ne "   $counter:  Waiting to conect to $camera_model on $camera_ip\r"
-        sleep 3
-  done
-
-  echo "Conecting to $camera_model on $camera_ip"
-  # just a message
-  echo "Tether photos to $store_photos_in"
-
-  # create the folder inside store_photo path dir and make the folder name
-  # as same as date
-  mkdir -p $store_photos_in
-
-  # killall process with gphoto to avoid error on device busy
-  # problem with claim device
-  # https://askubuntu.com/questions/993876/gphoto2-could-not-claim-the-usb-device
-  pkill -f gphoto2
-  pkill -f gphoto
 
 
-  #cd "$store_photos_in/tethered-$now" && gphoto2 --port ptpip:$camera_ip "--camera=$camera_model" --capture-tethered --hook-script=hook.sh
-  #cd "$store_photos_in/tethered-$now" && gphoto2 --port ptpip:$camare_ip --camera '$camera_model' --capture-tethered --hook-script=$HOME/Desktop/Applications/hook.sh
- cd "$store_photos_in" && gphoto2 --keep --port ptpip:$camera_ip --camera="$camera_model" --capture-tethered --hook-script=$HOME/Desktop/Applications/hook.sh
+echo "Conecting to $camera_model on $camera_ip"
+# just a message
+echo "Tether photos to $store_photos_in"
 
-  # go to  the new folder created and process capture-tethered
-  # just a message
-  echo "Tether photos to $store_photos_in"
-done
+# create the folder inside store_photo path dir and make the folder name
+# as same as date
+mkdir -p $store_photos_in
+
+# killall process with gphoto to avoid error on device busy
+# problem with claim device
+# https://askubuntu.com/questions/993876/gphoto2-could-not-claim-the-usb-device
+pkill -f gphoto2
+pkill -f gphoto
+
+
+cd "$store_photos_in" && gphoto2 --port ptpip:$camera_ip --camera="$camera_model" --capture-tethered --hook-script=$HOME/Desktop/Applications/hook.sh --keep
+#cd "$store_photos_in/tethered-$now" && gphoto2 --port ptpip:$camare_ip --camera '$camera_model' --capture-tethered --hook-script=$HOME/Desktop/Applications/hook.sh
+#cd "$store_photos_in" && gphoto2 --keep --port ptpip:$camera_ip --camera="$camera_model" --capture-tethered --hook-script=$HOME/Desktop/Applications/hook.sh
+
+# go to  the new folder created and process capture-tethered
+# just a message
+echo "Tether photos to $store_photos_in"
+
